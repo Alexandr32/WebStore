1165ee1ab8fb89feb40a91dc63532572607a09b5
Alexandr <alex3289@gmail.com>
2019-09-15 17:24:21 +0600

    Добавлен контроллер

---
+++ WebStore/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return Content("Ура все работает!");
+        }
+
+    }
+}
