3dc9c835af0627deb8666aa0b0d20dee70690329
Alexandr <alex3289@gmail.com>
2019-09-15 17:11:29 +0600

    Добавлены комментарии startup

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -12,27 +12,41 @@ namespace WebStore
 {
     public class Startup
     {
+        // В данном свойстве будет храниться конфигурация приложения
+        // на время прохождения процесса конфигурации
         /// <summary>
         /// Свойство для доступа к конфигурации
         /// </summary>
         public IConfiguration Configuration { get; }
 
+        // Конструктор класса вызывается инфраструктурой ASP.NET Core
+        // Инфраструктура переза выполнением конструктора исследует требуемый
+        // ему набор параметров и передаёт их ему в случае, если они ей
+        // известны. В противном случае будет сгенерировано исключение
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        // Данный метод вызывается инфраструктурой ASP.NET Core сразу
+        // после завершения работы конструктора.
+        // Его основное предназначение - добавить (объявить) все сервисы,
+        // которые впоследствии потребуются при работе приложения
         public void ConfigureServices(IServiceCollection services)
         {
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        // Данный метод вызвается инфраструктурой ASP.NET Core по завершении
+        // объявления сервисов. Его предназначение - сконфигурировать сервисы
+        public void Configure(
+            IApplicationBuilder app, // Объект с параметрами приложения
+            IHostingEnvironment env) // Объект с параметрами хоста
         {
+            // Используется ли режим отладки
             if (env.IsDevelopment())
             {
+                // Разрешаем отображать подробную страницу с информацией 
+                // о возникающем исключении
                 app.UseDeveloperExceptionPage();
             }
 
