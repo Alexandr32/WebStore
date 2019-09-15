c1c2b6180a09c8326b8f6b4e18dbe8235eb5b6eb
Alexandr <alex3289@gmail.com>
2019-09-15 17:35:51 +0600

    Добавлена модель

---
+++ WebStore/ViewsModels/EmployeeView.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.ViewsModels
+{
+    public class EmployeeView
+    {
+        public int Id { get; set; }
+        /// <summary>
+        /// Имя
+        /// </summary>
+        public string FirstName { get; set; }
+        /// <summary>
+        /// Фамилия
+        /// </summary>
+        public string SurName { get; set; }
+        /// <summary>
+        /// Отчество
+        /// </summary>
+        public string Patronymic { get; set; }
+        /// <summary>
+        /// Возраст
+        /// </summary>
+        public int Age { get; set; }
+        /// <summary>
+        /// Дата рождения
+        /// </summary>
+        public DateTime DateBirth { get; set; }
+        /// <summary>
+        /// Дата трудоустройства
+        /// </summary>
+        public DateTime DateEmployment { get; set; }
+    }
+}
