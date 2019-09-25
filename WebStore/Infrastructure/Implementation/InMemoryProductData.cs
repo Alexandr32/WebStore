e0143e835ddcdd1a00acb125bb444da7929c19b4
Alexandr <alex3289@gmail.com>
2019-09-25 20:17:37 +0600

    Добавлен список товаров

--- WebStore/Infrastructure/Implementation/InMemoryProductData.cs
+++ WebStore/Infrastructure/Implementation/InMemoryProductData.cs
@@ -420,16 +420,15 @@ namespace WebStore.Infrastructure.Implementation
         public IEnumerable<Product> GetProducts(ProductFilter filter)
         {
             var products = _products;
+
             if (filter.CategoryId.HasValue)
-            {
-                products = products.Where(p => p.CategoryId.Equals(filter.CategoryId)).ToList();
-            }
-                
+                products = products.Where(p =>
+                    p.CategoryId.Equals(filter.CategoryId)).ToList();
             if (filter.BrandId.HasValue)
-            {
-                products = products.Where(p => p.BrandId.HasValue && p.BrandId.Value.Equals(filter.BrandId.Value)).ToList();
-            }
-                
+                products = products.Where(p =>
+                        p.BrandId.HasValue && p.BrandId.Value.Equals(filter.BrandId.Value))
+                    .ToList();
+
             return products;
         }
     }
