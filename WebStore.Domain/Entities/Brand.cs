9851477769607e04224a583d846b0569ebf27543
Alexandr <alex3289@gmail.com>
2019-09-25 14:06:28 +0600

    Добавлена библиотека

---
+++ WebStore.Domain/Entities/Brand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebStore.Domain.Entities.Base;
+using WebStore.Domain.Entities.Base.Interfaces;
+
+namespace WebStore.Domain.Entities
+{
+    /// <summary>
+    /// Сущность бренд
+    /// </summary>
+    public class Brand : NamedEntity, IOrderedEntity
+    {
+        /// <summary>
+        /// Порядок
+        /// </summary
+        public int Order { get; set; }
+    }
+
+    /// <summary>
+    /// Сущность секция
+    /// </summary>
+    public class Section : NamedEntity, IOrderedEntity
+    {
+        /// <summary>
+        /// Родительская секция (при наличии)
+        /// </summary>
+        public int? ParentId { get; set; }
+
+        /// <summary>
+        /// Порядок
+        /// </summary
+        public int Order { get; set; }
+    }
+
+
+
+}
