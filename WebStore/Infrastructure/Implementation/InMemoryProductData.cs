df3beddf398d7520033269d498be6343e13c49a5
Alexandr <alex3289@gmail.com>
2019-09-29 18:10:33 +0600

    Исправлено про-во имен

--- WebStore/Infrastructure/Implementation/InMemoryProductData.cs
+++ WebStore/Infrastructure/Implementation/InMemoryProductData.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using WebStore.Domain.Entities;
-using WebStore.Domain.Filters;
+using WebStore.DomainNew.Entities;
+using WebStore.DomainNew.Filters;
 using WebStore.Infrastructure.Intefaces;
 
 namespace WebStore.Infrastructure.Implementation
