5e78207de440aea1816358973c79d43a451b69c8
Alexandr <alex3289@gmail.com>
2019-10-07 12:04:00 +0600

    Update RegisterUserViewModel.cs

--- WebStore/ViewsModels/RegisterUserViewModel.cs
+++ WebStore/ViewsModels/RegisterUserViewModel.cs
@@ -10,24 +10,24 @@ namespace WebStore.ViewsModels
     public class RegisterUserViewModel
     {
         // Имя пользователя
-        [Required(AllowEmptyStrings = false, ErrorMessage = "Поле является обязательным"), MaxLength(255)]
+        [Required(AllowEmptyStrings=false, ErrorMessage="Поле является обязательным"), MaxLength(255)]
         [Display(Name = "Имя пользователя")]
         public string UserName { get; set; }
 
         // Почта
         [Display(Name = "Электронная почта")]
-        [Required(AllowEmptyStrings = false, ErrorMessage = "Поле является обязательным"), DataType(DataType.EmailAddress)]
+        [Required(AllowEmptyStrings=false, ErrorMessage="Поле является обязательным"), DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
         // Пароль
-        [Required(AllowEmptyStrings = false, ErrorMessage = "Поле является обязательным"), DataType(DataType.Password)]
-        [Display(Name = "Пароль")]
+        [Required(AllowEmptyStrings=false, ErrorMessage="Поле является обязательным"), DataType(DataType.Password)]
+        [Display(Name="Пароль")]
         public string Password { get; set; }
 
         // Подтверждение пароля
         // Compare(nameof(Password)) - связывание с полем пароля
-        [Required(AllowEmptyStrings = false, ErrorMessage = "Поле является обязательным"), DataType(DataType.Password), Compare(nameof(Password), ErrorMessage = "Пароли не совпадает")]
-        [Display(Name = "Подтверждение пароля")]
+        [Required(AllowEmptyStrings=false, ErrorMessage="Поле является обязательным"), DataType(DataType.Password), Compare(nameof(Password), ErrorMessage="Пароли не совпадает")]
+        [Display(Name="Подтверждение пароля")]
         public string ConfirmPassword { get; set; }
     }
 }
